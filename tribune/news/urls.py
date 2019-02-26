fb0fe3267eac69ad4034485337af5af8cf288644
Fahari <karonjekevin67@gmail.com>
2019-02-26 11:39:01 +0300

    created a new article

--- tribune/news/urls.py
+++ tribune/news/urls.py
@@ -3,7 +3,7 @@ from . import views
 # from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.news_of_day,name='newsToday'),
+    # url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.news_of_day,name='newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
 ]
