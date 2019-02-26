2fbc4d25c4551fb9fbbb60c4547ca997281e0674
Fahari <karonjekevin67@gmail.com>
2019-02-26 17:17:48 +0300

    successfully updated views

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,37 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
-# def convert_dates(dates):
-#
-#     # Function that gets the weekday number for the date.
-#     day_number = dt.date.weekday(dates)
-#
-#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-#
-#     # Returning the actual day of the week
-#     day = days[day_number]
-#     return day
 
 
 def news_of_day(request):
     date = dt.date.today()
     return render(request, 'all-news/today-news.html', {"date": date,})
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    # day = convert_dates(date)
-    #
-    # html = f'''
-    #     <html>
-    #         <body>
-    #             <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-    #         </body>
-    #     </html>
-    #         '''
-    # return HttpResponse(html)
 
 def past_days_news(request,past_date):
 
@@ -49,12 +29,7 @@ def past_days_news(request,past_date):
 
     return render(request, 'all-news/past-news.html', {"date": date})
 
-    # day = convert_dates(date)
-    # html = f'''
-    #     <html>
-    #         <body>
-    #             <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-    #         </body>
-    #     </html>
-    #         '''
-    # return HttpResponse(html)
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
