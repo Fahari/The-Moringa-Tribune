769725d5716c4ac091ed4da1ba3cdc5f26f91886
Fahari <karonjekevin67@gmail.com>
2019-02-27 10:56:37 +0300

    successfully upload images

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -32,6 +32,8 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
+
 
 
     @classmethod
