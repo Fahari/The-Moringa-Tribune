c65cd3f1709a0766f9fb01612685f6c425e119f2
Fahari <karonjekevin67@gmail.com>
2019-03-01 11:18:36 +0300

    complete 2

--- news/views.py
+++ news/views.py
@@ -9,6 +9,11 @@ def welcome(request):
 
 
 
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
 def past_days_news(request,past_date):
 
     try:
@@ -22,13 +27,9 @@ def past_days_news(request,past_date):
 
     if date == dt.date.today():
         return redirect(news_today)
+    news=Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date": date, "news":news})
 
-    return render(request, 'all-news/past-news.html', {"date": date})
-
-def news_today(request):
-    date = dt.date.today()
-    news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 def search_results(request):
 
